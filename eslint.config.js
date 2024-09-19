f6b1c00e9493fdd2bd65b37367e1a4622f33dceb
Ana Nikolic <Ana_Nikolic@sluz.ch>
2024-09-19 10:47:18 +0200

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+    {
+      rules: {
+        eqeqeq: 'error',           // Erzwingt die Verwendung von '===' statt '=='
+        'no-unused-vars': 'error', // Zeigt Fehler für ungenutzte Variablen an
+      },
+    },
+  ];
+  
