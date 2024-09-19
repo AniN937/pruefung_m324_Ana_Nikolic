67cc828fdd48bafdd11cd8a49829e08580d6796b
Ana Nikolic <Ana_Nikolic@sluz.ch>
2024-09-19 10:54:58 +0200

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 export default [
-    {
-      rules: {
-        eqeqeq: 'error',           // Erzwingt die Verwendung von '===' statt '=='
-        'no-unused-vars': 'error', // Zeigt Fehler für ungenutzte Variablen an
+  {
+    rules:
+      {
+        eqeqeq:
+          'error', // Erzwingt die Verwendung von '===' statt '=='
+        'no-unused-vars':
+          'error', // Zeigt Fehler für ungenutzte Variablen an
       },
-    },
-  ];
-  
+  },
+];
